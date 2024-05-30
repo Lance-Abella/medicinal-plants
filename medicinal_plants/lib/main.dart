8d1cf1504ce122b4192e49a1322d88657e07034d
Lance Mathew Abella <abellalance2@gmail.com>
2024-05-30 21:38:11 +0800

    working on landing screen

---
+++ medicinal_plants/lib/main.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:medicinal_plants/src/screens/landing-screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LandingScreen(),
+      routes: {
+
+      },
+    );
+  }
+}
+
+
